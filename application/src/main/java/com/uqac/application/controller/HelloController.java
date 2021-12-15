8e7f5072aa7f6f5e6c8ec6c7bb039b2637a504f9
SeanAnica <37900784+SeanAnica@users.noreply.github.com>
2021-12-15 18:42:50 -0500

    Update HelloController.java

--- application/src/main/java/com/uqac/application/controller/HelloController.java
+++ application/src/main/java/com/uqac/application/controller/HelloController.java
@@ -1,17 +1,12 @@
 package com.uqac.application.controller;
 
-import com.uqac.application.model.ResponseTemplateVO;
-import com.uqac.application.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
